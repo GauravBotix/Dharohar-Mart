55f20d2909da32353ad51625ddffa2b016fe784e
Gaurav_Jha <jhagaurav82003@gmail.com>
2025-06-03 22:27:51 +0530

    changes made in db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,19 +1,19 @@
 import mongoose from "mongoose";
 
-if (process.env.MONGODB_URI) {
-    throw new Error('Please connect to the database by providing the correct database uri.')
+if (!process.env.MONGODB_URI) {
+  throw new Error(
+    "Please connect to the database by providing the correct database uri."
+  );
 }
 
-const connectDb = async ()=>{
-    try {
-        await mongoose.connect(process.env.MONGODB_URI);
-        console.log('Connected to DB.');
-        
-    } catch (error) {
-        console.log('MongoDb connection error occured:', error);
-        process.exit(1);
-        
-    }
-}
+const connectDb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("Connected to DB.");
+  } catch (error) {
+    console.log("MongoDb connection error occured:", error);
+    process.exit(1);
+  }
+};
 
 export default connectDb;
